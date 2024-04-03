54c9ef0139ccf26a108825a698f24de394badb48
Hadeer Moustafa <hadeer.moustafa@outlook.com>
2024-04-03 20:10:29 +0200

    fix code

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 "use strict";
-<<<<<<< HEAD
+
 let random = Math.trunc(Math.random() * 20) + 1;
-=======
-const random = Math.trunc(Math.random() * 20) + 1;
-console.log(random);
->>>>>>> 83cef9b06f07d3ac3745c3b654f76b4b7fa7e286
+
 let score = 20;
 let highscore = 0;
 
@@ -18,11 +15,9 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = guess;
     document.querySelector("body").style.backgroundColor = "green";
     document.querySelector(".number").style.width = "30rem";
-<<<<<<< HEAD
+
     document.querySelector(".highscore").value = score;
     document.querySelector(".highscore").textContent = score;
-=======
->>>>>>> 83cef9b06f07d3ac3745c3b654f76b4b7fa7e286
   } else if (guess > random) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
@@ -49,17 +44,8 @@ document.querySelector(".check").addEventListener("click", function () {
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   document.querySelector(".score").textContent = 20;
-<<<<<<< HEAD
   random = Math.trunc(Math.random() * 20) + 1;
   document.querySelector(".message").textContent = "Start guessing...";
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
-=======
-  //random = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector(".message").textContent = "Start guessing...";
-  document.querySelector(".number").textContent = "?";
-  document.querySelector(".guess").textContent = "";
->>>>>>> 83cef9b06f07d3ac3745c3b654f76b4b7fa7e286
-  document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector(".number").style.width = "15rem";
 });
